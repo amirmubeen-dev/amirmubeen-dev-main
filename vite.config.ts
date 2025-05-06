49a26065839b537005a6c961a2db72f61c12b98f
amirmubeen-dev <amirmubeen.mw@gmail.com>
2025-05-06 12:33:12 +0500

    chage deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/amirmubeen-dev-main/', // 👈 this enables GitHub Pages deployment
+  base: '/amirmubeen-dev-main/', // 👈 Set this to your GitHub repo name
   server: {
     host: "::",
     port: 8080,
